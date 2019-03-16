f44d642ace44458eb388a47905b3da9f48b0745a
Lidiane <alencar.lidiane@gmail.com>
2019-03-16 15:47:33 -0300

    Tudo junto e misturado

---
+++ app/src/main/java/br/com/app/geeksguide/MapsActivity.kt
@@ -0,0 +1,37 @@
+package br.com.app.geeksguide
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val lins = LatLng(-23.57, -46.62)
+        mMap.addMarker(MarkerOptions().position(lins).title("Geek's Guide"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(lins))
+        mMap.setMinZoomPreference(18.0f)
+
+    }
+}
---
+++ app/src/main/java/br/com/app/geeksguide/SobreActivity.kt
@@ -0,0 +1,80 @@
+package br.com.app.geeksguide
+
+import android.Manifest
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+import android.support.v4.app.ActivityCompat
+import android.widget.Button
+import android.widget.Toast
+
+import android.view.View
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.support.annotation.NonNull
+import android.support.v4.content.ContextCompat.startActivity
+
+
+class SobreActivity : AppCompatActivity() {
+
+    private val REQUEST_CALL = 1
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sobre)
+
+
+        val btFind = findViewById<Button>(R.id.btFind)
+        val btCall = findViewById<Button>(R.id.btCall)
+        val btShare = findViewById<Button>(R.id.btShare)
+
+        btFind.setOnClickListener{
+            //Toast.makeText(this, "Hi there! This is a Toast.", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        //FAZER LIGACAO
+        btCall.setOnClickListener{
+            makePhoneCall();
+        }
+
+
+        //COMPARTILHAR CONTEUDO
+        btShare.setOnClickListener{
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.type="text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Conheca nosso APP. Geeks Guide - Disponivel na Google Play");
+            startActivity(Intent.createChooser(shareIntent,getString(R.string.shareAPPTitle)))
+        }
+
+
+    }
+
+
+    private fun makePhoneCall() {
+
+        if (ContextCompat.checkSelfPermission(this@SobreActivity,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this@SobreActivity,
+                    arrayOf(Manifest.permission.CALL_PHONE), REQUEST_CALL)
+        } else {
+            val dial = "tel:11977016397"
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse(dial)))
+        }
+
+
+    }
+
+
+
+
+
+
+}
