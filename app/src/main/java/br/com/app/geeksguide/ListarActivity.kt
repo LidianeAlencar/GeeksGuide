e8fc33fb740e5ecb10eb6d95247e1b6a0f7f6659
Lidiane <alencar.lidiane@gmail.com>
2019-03-17 19:26:00 -0300

    menu

--- app/src/main/java/br/com/app/geeksguide/ListarActivity.kt
+++ app/src/main/java/br/com/app/geeksguide/ListarActivity.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import br.com.app.geeksguide.model.Local
